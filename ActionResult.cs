237c1e84cde92e2e7e0ab5b2a35ce2de15d4b453
rekar <rekarpc98@gmail.com>
2024-03-13 23:50:16 +0300

    Add Result extensions
    Add extension methods for Result<T> and Result types to easily be converted back to IActionResult based on success of failure

---
+++ ActionResult.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Dynamic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreKit;
+
+public class ActionResult
+{
+	private readonly JsonSerializerOptions jsonSerializerOptions = new();
+
+	public ActionResult()
+	{
+		this.jsonSerializerOptions.DictionaryKeyPolicy ??= this.jsonSerializerOptions.PropertyNamingPolicy;
+	}
+
+	public NotFoundObjectResult NotFound(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status404NotFound,
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new NotFoundObjectResult(problemDetail);
+	}
+	
+	public BadRequestObjectResult BadRequest(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status400BadRequest,
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new BadRequestObjectResult(problemDetail);
+	}
+	
+	public ConflictObjectResult Conflict(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status409Conflict,
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new ConflictObjectResult(problemDetail);
+	}
+	
+	public UnauthorizedObjectResult Unauthorized(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status401Unauthorized,
+			Type = "https://tools.ietf.org/html/rfc7235#section-3.1",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new UnauthorizedObjectResult(problemDetail);
+	}
+	
+	public ObjectResult Forbidden(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status403Forbidden,
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new ObjectResult(problemDetail) { StatusCode = StatusCodes.Status403Forbidden };
+	}
+	
+	public ObjectResult InternalServerError(Exception exception)
+	{
+		var problemDetail = new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status500InternalServerError,
+			Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+			Title = exception.Message
+		};
+
+		MapExceptionDataToProblemDetail(exception, problemDetail);
+
+		return new ObjectResult(problemDetail) { StatusCode = StatusCodes.Status500InternalServerError };
+	}
+
+	private void MapExceptionDataToProblemDetail(Exception exception, ValidationProblemDetails problemDetail)
+	{
+		foreach (DictionaryEntry error in exception.Data)
+		{
+			string errorKey = ApplySerialization(error, jsonSerializerOptions);
+			problemDetail.Errors.Add(key: errorKey, value: ((List<string>)error.Value)?.ToArray());
+		}
+	}
+
+	private static string ApplySerialization(DictionaryEntry error, JsonSerializerOptions jsonSerializerOptions)
+	{
+		IDictionary<string, object> interimObject = new ExpandoObject();
+		interimObject.Add(error.Key.ToString(), error.Value);
+		string serialisedInterimObject = JsonSerializer.Serialize(interimObject, jsonSerializerOptions);
+
+		var deserializedError =
+			JsonSerializer.Deserialize<IDictionary<string, Object>>(serialisedInterimObject, jsonSerializerOptions);
+
+		return deserializedError.Keys.First();
+	}
+}
+
+public class ValidationProblemDetails
+{
+	public string Title { get; set; } = default!;
+	public IDictionary<string, string[]> Errors { get => Data; set => Data = new Dictionary<string, string[]>(value); }
+	public Dictionary<string, string[]> Data { get; set; } = new(StringComparer.Ordinal);
+	public string Type { get; set; } = default!;
+	public int Status { get; set; }
+}
---
+++ Helpers/ResultExtensions.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreKit.Commons;
+using NetCoreKit.Exceptions;
+
+namespace NetCoreKit.Helpers;
+
+/// <summary>
+/// Provides extension methods for <see cref="Result{T}"/> to easily convert to IActionResult types based on success or failure.
+/// </summary>
+public static class ResultExtensions
+{
+	/// <summary>
+	/// Converts a <see cref="Result{T}"/> to an OkObjectResult if the operation was successful, otherwise returns an error response.
+	/// </summary>
+	/// <typeparam name="T">The type of the result's value.</typeparam>
+	/// <param name="result">The operation result.</param>
+	/// <returns>An IActionResult representing an OK response with the result's value or an error response.</returns>
+	public static IActionResult Ok<T>(this Result<T> result)
+	{
+		return result.IsSuccess ? new OkObjectResult(result.Value) : result.Exception.ReturnErrorResponse();
+	}
+	
+	/// <summary>
+	/// Converts a <see cref="Result"/> to an OkObjectResult if the operation was successful, otherwise returns an error response.
+	/// </summary>
+	/// <param name="result">The operation result.</param>
+	/// <returns>An IActionResult representing an OK response or an error response.</returns>
+	public static IActionResult Ok(this Result result)
+	{
+		return result.IsSuccess ? new OkObjectResult(null) : result.Exception.ReturnErrorResponse();
+	}
+
+	/// <summary>
+	/// Converts a <see cref="Result{T}"/> to a NoContentResult if the operation was successful, otherwise returns an error response.
+	/// </summary>
+	/// <typeparam name="T">The type of the result's value.</typeparam>
+	/// <param name="result">The operation result.</param>
+	/// <returns>An IActionResult representing a no content response or an error response.</returns>
+	public static IActionResult NoContent<T>(this Result<T> result)
+	{
+		return result.IsSuccess ? new NoContentResult() : result.Exception.ReturnErrorResponse();
+	}
+	
+	/// <summary>
+	/// Converts a <see cref="Result"/> to a NoContentResult if the operation was successful, otherwise returns an error response.
+	/// </summary>
+	/// <param name="result">The operation result.</param>
+	/// <returns>An IActionResult representing a no content response or an error response.</returns>
+	public static IActionResult NoContent(this Result result)
+	{
+		return result.IsSuccess ? new NoContentResult() : result.Exception.ReturnErrorResponse();
+	}
+
+	/// <summary>
+	/// Returns an error response based on the type of exception contained in the result.
+	/// </summary>
+	/// <typeparam name="T">The type of the result's value.</typeparam>
+	/// <param name="result">The operation result containing the exception.</param>
+	/// <returns>An IActionResult representing an error response.</returns>
+	public static IActionResult ReturnResponse<T>(this Result<T> result)
+	{
+		return result.Exception.ReturnErrorResponse();
+	}
+	
+	/// <summary>
+	/// Returns an error response based on the type of exception contained in the result.
+	/// </summary>
+	/// <param name="result">The operation result containing the exception.</param>
+	/// <returns>An IActionResult representing an error response.</returns>
+	public static IActionResult ReturnResponse(this Result result)
+	{
+		return result.Exception.ReturnErrorResponse();
+	}
+
+	/// <summary>
+	/// Determines the appropriate IActionResult to return based on the type of exception.
+	/// </summary>
+	/// <param name="exception">The exception that occurred.</param>
+	/// <returns>An IActionResult representing the specific error based on the exception type.</returns>
+	private static IActionResult ReturnErrorResponse(this Exception exception)
+	{
+		switch (exception)
+		{
+			case NotFoundException:
+			{
+				return new ActionResult().NotFound(exception);
+			}
+			case InvalidException:
+			{
+				return new ActionResult().BadRequest(exception);
+			}
+			case ConflictException:
+			{
+				return new ActionResult().Conflict(exception);
+			}
+			case UnauthorizedException:
+			{
+				return new ActionResult().Unauthorized(exception);
+			}
+			case ForbiddenException:
+			{
+				return new ActionResult().Forbidden(exception);
+			}
+			case InternalServerErrorException:
+			{
+				return new ActionResult().InternalServerError(exception);
+			}
+			default:
+				return new StatusCodeResult(500);
+		}
+	}
+}
