293c2bfaaa6fba52cd561676e4f42dff6e04e980
rekar <rekarpc98@gmail.com>
2024-03-14 19:49:53 +0300

    Remove ValidationProblemDetails custom type

--- ActionResult.cs
+++ ActionResult.cs
@@ -28,7 +28,7 @@ public class ActionResult
 
 		return new NotFoundObjectResult(problemDetail);
 	}
-	
+
 	public BadRequestObjectResult BadRequest(Exception exception)
 	{
 		var problemDetail = new ValidationProblemDetails
@@ -42,7 +42,7 @@ public class ActionResult
 
 		return new BadRequestObjectResult(problemDetail);
 	}
-	
+
 	public ConflictObjectResult Conflict(Exception exception)
 	{
 		var problemDetail = new ValidationProblemDetails
@@ -56,7 +56,7 @@ public class ActionResult
 
 		return new ConflictObjectResult(problemDetail);
 	}
-	
+
 	public UnauthorizedObjectResult Unauthorized(Exception exception)
 	{
 		var problemDetail = new ValidationProblemDetails
@@ -70,7 +70,7 @@ public class ActionResult
 
 		return new UnauthorizedObjectResult(problemDetail);
 	}
-	
+
 	public ObjectResult Forbidden(Exception exception)
 	{
 		var problemDetail = new ValidationProblemDetails
@@ -84,7 +84,7 @@ public class ActionResult
 
 		return new ObjectResult(problemDetail) { StatusCode = StatusCodes.Status403Forbidden };
 	}
-	
+
 	public ObjectResult InternalServerError(Exception exception)
 	{
 		var problemDetail = new ValidationProblemDetails
@@ -104,28 +104,19 @@ public class ActionResult
 		foreach (DictionaryEntry error in exception.Data)
 		{
 			string errorKey = ApplySerialization(error, jsonSerializerOptions);
-			problemDetail.Errors.Add(key: errorKey, value: ((List<string>)error.Value)?.ToArray());
+			problemDetail.Errors.Add(key: errorKey, value: ((List<string>)error.Value!).ToArray());
 		}
 	}
 
 	private static string ApplySerialization(DictionaryEntry error, JsonSerializerOptions jsonSerializerOptions)
 	{
-		IDictionary<string, object> interimObject = new ExpandoObject();
-		interimObject.Add(error.Key.ToString(), error.Value);
+		IDictionary<string, object> interimObject = new ExpandoObject()!;
+		interimObject.Add(error.Key.ToString()!, error.Value!);
 		string serialisedInterimObject = JsonSerializer.Serialize(interimObject, jsonSerializerOptions);
 
 		var deserializedError =
-			JsonSerializer.Deserialize<IDictionary<string, Object>>(serialisedInterimObject, jsonSerializerOptions);
+			JsonSerializer.Deserialize<IDictionary<string, object>>(serialisedInterimObject, jsonSerializerOptions)!;
 
 		return deserializedError.Keys.First();
 	}
-}
-
-public class ValidationProblemDetails
-{
-	public string Title { get; set; } = default!;
-	public IDictionary<string, string[]> Errors { get => Data; set => Data = new Dictionary<string, string[]>(value); }
-	public Dictionary<string, string[]> Data { get; set; } = new(StringComparer.Ordinal);
-	public string Type { get; set; } = default!;
-	public int Status { get; set; }
 }
