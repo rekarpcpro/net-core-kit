940a994cea779e85ee782a8537b6c7472ea8f2bf
rekar <rekarpc98@gmail.com>
2024-03-21 23:40:00 +0300

    Add task extensions
    This commit adds `ThrowIfValueNull<T>` extension methods for `Task<T?>`, enabling automatic null checks and exception throwing for task results. It supports custom exception messages and includes an overload for contextual inputs. These methods simplify error handling in async code paths.

---
+++ Extensions/TaskExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace NetCoreKit.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="Task{T}"/>.
+/// </summary>
+public static class TaskExtensions
+{
+	/// <summary>
+	/// Throws an exception if the task's result is null.
+	/// </summary>
+	/// <typeparam name="T">The type of the task's result.</typeparam>
+	/// <param name="task">The task to await.</param>
+	/// <param name="message">The optional message for the exception. If null, a default message is used.</param>
+	/// <returns>The task's result if it is not null.</returns>
+	/// <exception cref="Exception">Thrown when the task's result is null.</exception>
+	public static async Task<T> ThrowIfResultNull<T>(this Task<T?> task, string? message = null)
+	{
+		T? result = await task;
+
+		if (result == null)
+		{
+			throw new Exception(message ?? $"{typeof(T).Name} is null");
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// Throws an exception if the task's result is null, including provided inputs in the exception message.
+	/// </summary>
+	/// <typeparam name="T">The type of the task's result.</typeparam>
+	/// <param name="task">The task to await.</param>
+	/// <param name="inputs">The inputs to include in the exception message if the task's result is null.</param>
+	/// <returns>The task's result if it is not null.</returns>
+	/// <exception cref="Exception">Thrown when the task's result is null, with a message that includes the provided inputs.</exception>
+	public static async Task<T> ThrowIfResultNull<T>(this Task<T?> task, params object[] inputs)
+	{
+		T? result = await task;
+
+		if (result == null)
+		{
+			throw new Exception(
+				string.Format($"{typeof(T).Name} is null, inputs:", string.Join(separator: ", ", inputs))
+			);
+		}
+
+		return result;
+	}
+}
